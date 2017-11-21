bc02e71c0bbaef2c72a911dcc660768cdb19930d
L.P <parisot.laurent@gmail.com>
2017-11-21 09:47:57 +0100

    Reservation form

--- app/app.routing.ts
+++ app/app.routing.ts
@@ -8,6 +8,7 @@ import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
 import { FavoritesComponent } from './favorites/favorites.component';
+import { ReservationComponent } from './reservation/reservation.component';
 
 const routes: Routes = [
     { path: "", redirectTo: "/home", pathMatch: "full" },
@@ -16,7 +17,8 @@ const routes: Routes = [
     { path: "menu", component: MenuComponent },
     { path: "contact", component: ContactComponent },
     { path: 'dishdetail/:id', component: DishdetailComponent },
-    { path: "favorites", component: FavoritesComponent }
+    { path: "favorites", component: FavoritesComponent },
+    { path: "reservation", component: ReservationComponent }
 ];
 
 @NgModule({
